b853ac831ad13041196498fea84dbaf747ac8f92
gegedenice <geraldine.geoffroy@univ-cotedazur.fr>
2022-02-21 10:06:58 +0100

    refactor functions

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ import dash_bootstrap_components as dbc
 import visdcc
 import logging
 import math
+import functions as fn
 import config
 
 # config variables
@@ -61,210 +62,6 @@ COLORS = {'VALID': "#FFB300",  # Vivid Yellow
           'laboratory': "#F6768E",  # Strong Purplish Pink
           'department': "#F13A13",  # Vivid Reddish Orange
           'researchteam': "#7F180D", }  # Strong Reddish Brown
-
-# FUNCTIONS
-def render_network_options(hierarchical_enabled=False, direction='UD'):
-    DEFAULT_OPTIONS = {
-        'height': '700px',
-        'width': '100%',
-        'interaction': {'hover': True, 'hoverConnectedEdges': True, 'navigationButtons': True},
-        'edges': {'arrows': {
-            'to': {'enabled': True, 'scaleFactor': 1, 'type': "arrow"}
-        }},
-        'layout': {'hierarchical': {
-            'enabled': hierarchical_enabled,
-            'levelSeparation': 150,
-            'nodeSpacing': 100,
-            'treeSpacing': 200,
-            'blockShifting': True,
-            'edgeMinimization': True,
-            'parentCentralization': True,
-            'direction': direction,        # UD, DU, LR, RL
-            'sortMethod': 'directed',  # hubsize, directed
-            # 'shakeTowards': 'roots'  # roots, leaves
-        }
-        },
-        # 'edges': {'scaling': {'min': 1, 'max': 5}},
-        'physics': {'stabilization': {'iterations': 100}}
-    }
-    return DEFAULT_OPTIONS
-
-def render_network_legend(colors_dict):
-    c_list = []
-    for key, value in colors_dict.items():
-        c_list.append(dbc.Badge(key, color=value, className="me-1"))
-    return ",".join(c_list)
-
-
-def render_datatable(columns, data):
-    return html.Div([dt.DataTable(
-        columns=columns,
-        data=data,
-        sort_action="native",
-        sort_mode="multi",
-        filter_action='native',
-        page_action="native",
-        page_current=0,
-        page_size=10,
-        style_header={
-            "backgroundColor": "rgb(2,21,70)", "color": "white", "textAlign": "center", },
-        style_table={'overflowX': 'auto'},
-        style_data={"whiteSpace": "normal"},
-        style_cell={
-            "padding": "5px",
-            "midWidth": "0px",
-            "width": "25%",
-            "textAlign": "left",
-            "border": "white",
-            'overflow': 'hidden',
-            'textOverflow': 'ellipsis',
-        },
-    ),
-    ]
-    )
-
-def get_nb_pub_by_struct(id):
-    numfound = ""
-    url = 'https://api.archives-ouvertes.fr/search/?wt=json&q=authStructId_i:{}&rows=1'.format(id)
-    resp = requests.get(url).text
-    if json.loads(resp)['response']:
-        numfound = json.loads(resp)['response']['numFound']
-    else:
-        pass
-    return numfound
-
-def get_child_struct(id, result=None):
-    """
-    Function to recursivly get all the child structures of a structure in the Aurehal referential.
-    Metadata are coming from a request on the parentDocid_id field to the ref/structure HAL API (param &q=parentDocid_i:).
-
-    Args
-    ----------
-    id (str|int) : the docid HAL structure identifier
-    result (list of dicts, default None) : the cumulative list of dictionaries in which the parsed data are incremented
-
-    Return
-    -------
-    returns a list of dicts populated with HAL docid (of structures) and "from" and "to" keys
-    Example : [{'from': 1039632, 'to': 520677}, {'from': 1039632, 'to': 537646},...] 
-
-    Uses
-    -------
-    * get_childStruct(id,None)
-    * Assign to a dataframe : df = pd.DataFrame(get_childStruct(id,None))
-    """
-    if result is None:  # create a new result if no intermediate was given
-        result = []
-    url = 'https://api.archives-ouvertes.fr/ref/structure/?wt=json&rows=10000&q=parentDocid_i:{}&fl=docid'.format(
-        id)
-    print(url)
-    resp = requests.get(url).text
-    if json.loads(resp)['response']['docs']:
-        data = json.loads(resp)['response']['docs']
-        for node in data:
-            result.append({"from": id, 'to': node['docid']})
-            get_child_struct(node['docid'], result)
-    else:
-        pass
-    # dedup in case of duplicate relatioships
-    output = [i for n, i in enumerate(result) if i not in result[n + 1:]]
-    return output
-
-
-def get_parent_struct(id, result=None):
-    """
-    Function to recursivly get all the parent structures of a structure in the Aurehal referential.
-    Metadata are coming from a request on the docid field to the ref/structure HAL API (param &q=docid:) and with the parentDocid_i in the result displayed fields (param &fl=parentDocid_i).
-
-    Args
-    ----------
-    id (str|int) : the docid HAL structure identifier
-    result (list of dicts, default None) : the cumulative list of dictionaries in which the parsed data are incremented
-
-    Return
-    -------
-    returns a list of dicts populated with HAL docid (of structures) and "from" and "to" keys
-    Example : [{'from': '409', 'to': 399760}, {'from': '117617', 'to': '409'}, {'from': '523042', 'to': '117617'}, {'from': '441569', 'to': '409'}, {'from': '1039632', 'to': '409'}]
-
-    Uses
-    -------
-    * get_parentStruct(id,None)
-    * Assign to a dataframe : df = pd.DataFrame(get_parentStruct(id,None))
-    """
-    if result is None:  # create a new result if no intermediate was given
-        result = []
-    url = 'https://api.archives-ouvertes.fr/ref/structure/?wt=json&q=docid:"{}"&fl=parentDocid_i'.format(
-        id)
-    print(url)
-    resp = requests.get(url).text
-    if json.loads(resp)['response']['docs'][0]:
-        data = json.loads(resp)['response']['docs'][0]['parentDocid_i']
-        for node in data:
-            result.append({"from": node, 'to': id})
-            get_parent_struct(node, result)
-    else:
-        pass
-    output = [i for n, i in enumerate(result) if i not in result[n + 1:]]
-    return output
-
-
-def get_struct_infos(id):
-    """
-    Function to get descriptive HAL metadata of a given structure.
-    Metadata are coming from a request on the docid field to the ref/structure HAL API (&q=docid:)
-    The choosen metadata for output are : acronym_s,label_s,valid_s,type_s,address_s,url_s
-
-    Args
-    ----------
-    id (str|int) : the docid HAL structure identifier
-
-    Return
-    -------
-    returns a dict populated with the HAL requested metadata
-    Example : {'id': 1039632,'label_s': "Université Côte d'Azur [UCA]",'acronym_s': 'UCA','address_s': 'Parc Valrose, 28, avenue Valrose 06108 Nice Cedex 2','url_s': 'https://univ-cotedazur.fr','type_s': 'regroupinstitution','valid_s': 'VALID'}
-
-    Uses
-    -------
-    * get_struct_infos(id)
-    * used in the get_list_struct_infos function
-    """
-    result = {}
-    url = 'https://api.archives-ouvertes.fr/ref/structure/?wt=json&q=docid:{}&fl=acronym_s,label_s,valid_s,type_s,address_s,url_s'.format(
-        id)
-    print(url)
-    resp = requests.get(url).text
-    data = json.loads(resp)['response']['docs']
-    result["id"] = id
-    result["nb_publis"] = get_nb_pub_by_struct(id)
-    for item in data:
-        for key, value in item.items():
-            result[key] = value
-    return result
-
-
-def get_list_struct_infos(docid_list):
-    """
-    applies the get_struct_infos function to a list of structure's docid and compile the results in a dataframe.
-
-    Args
-    ----------
-    docid_list (list) : a list of docid HAL structure's identifiers
-    Example : ["1039632","399760","409"]
-
-    Return
-    -------
-    returns a dataframe populated with all the results of teh get_struct_infos function
-
-    Uses
-    -------
-    * get_list_struct_infos(docid_list)
-    * assign to a new dataframe : df = get_list_struct_infos(docid_list)
-    """
-    results = pd.DataFrame()
-    for i in docid_list:
-        df = pd.DataFrame(get_struct_infos(i), index=[i])
-        results = pd.concat([results, df], axis=0).reset_index(drop=True)
-    return results
 
 # LAYOUT COMPONENTS
 # navbar github right button
@@ -571,9 +368,27 @@ app.layout = dbc.Container(
     ],
 )
 
+# SOME HELPERS FUNCTION
+def update_node_size(radio_nodes_size,node):
+    if radio_nodes_size == "non":
+         size = 7
+    else:
+        if node['nb_publis'] != 0:
+            size = round(math.log(int(node['nb_publis'])*5))
+        else:
+            size = 5
+    return size
+
+def filter_condition(input_filter_node_title,checklist_valid_s_colors, checklist_type_s_colors,node,title):
+    condition = (node["valid_s"] in checklist_valid_s_colors) & (
+                    node["type_s"] in checklist_type_s_colors)
+    if input_filter_node_title is not None:
+        condition = (node["valid_s"] in checklist_valid_s_colors) & (
+                    node["type_s"] in checklist_type_s_colors) & (str(input_filter_node_title) in title)
+    return condition
+
+
 # CALLBACKS
-
-
 @app.callback(Output('edge-dict', 'data'),
               Output('node-dict', 'data'),
               Output('node-table', 'children'),
@@ -587,19 +402,18 @@ def update_states(docid, select_harvest_direction, n_clicks):
         return dash.no_update
     elif trig_id == "submit-button":
         if select_harvest_direction == "desc":
-            edge_df = pd.DataFrame(get_child_struct(docid, None))
+            edge_df = pd.DataFrame(fn.get_child_struct(docid, None))
         elif select_harvest_direction == "asc":
-            edge_df = pd.DataFrame(get_parent_struct(docid, None))
+            edge_df = pd.DataFrame(fn.get_parent_struct(docid, None))
         if not edge_df.empty:
             list_unique_docid = set(edge_df["from"].unique().tolist()+edge_df["to"].unique().tolist())
-            node_df = get_list_struct_infos(list_unique_docid)
+            node_df = fn.get_list_struct_infos(list_unique_docid)
             columns = [{"name": str(i), "id": str(i)} for i in node_df.columns]
-            return edge_df.to_dict(orient='records'), node_df.to_dict(orient='records'), render_datatable(columns, node_df.to_dict(orient='records'))
+            return edge_df.to_dict(orient='records'), node_df.to_dict(orient='records'), fn.render_datatable(columns, node_df.to_dict(orient='records'))
         else:
             return None, None, None
     else:
         return None, None, None
-
 
 @app.callback(Output('network', 'children'),
               [Input("radio-nodes-color", "value"),
@@ -614,36 +428,19 @@ def update_states(docid, select_harvest_direction, n_clicks):
               prevent_initial_call=True
               )
 def render_network(radio_nodes_color, radio_nodes_size, checklist_valid_s_colors, checklist_type_s_colors, radio_hierarchical_enabled, select_hierarchical_direction, input_filter_node_title, edge_dict, node_dict):
-    #trig_id = dash.callback_context.triggered
-    #print(trig_id)
     edges = []
     nodes = []
     if (node_dict is not None) & (edge_dict is not None):
-        OPTIONS = render_network_options(
+        OPTIONS = fn.render_network_options(
             hierarchical_enabled=radio_hierarchical_enabled, direction=select_hierarchical_direction)
         for row in edge_dict:
             edges.append({**row, **{'id': str(row['from']) + "__" + str(row['to']),  'color': {'color': '#97C2FC'}}})
         for node in node_dict:
             #node title for tooltip
             title = '{} (id:{}) ({} publis) ({})'.format(node['label_s'], node['id'],node['nb_publis'],node['valid_s'])
-            #nodes sizes consitions
-            if radio_nodes_size == "non":
-                size = 7
-            else:
-                if node['nb_publis'] != 0:
-                    size = round(math.log(int(node['nb_publis'])*5))
-                else:
-                    size = 5
-            #nodes colors conditions
-            if input_filter_node_title is None:
-                condition = (node["valid_s"] in checklist_valid_s_colors) & (
-                    node["type_s"] in checklist_type_s_colors)
-            else:
-                condition = (node["valid_s"] in checklist_valid_s_colors) & (
-                    node["type_s"] in checklist_type_s_colors) & (str(input_filter_node_title) in title)
-            if condition:
+            if filter_condition(input_filter_node_title,checklist_valid_s_colors, checklist_type_s_colors,node,title):
                 nodes.append({**node, **{'label': node['acronym_s'], 'shape': 'dot',
-                             'size': size, 'title': title, 'color': COLORS[node[radio_nodes_color]]}})
+                             'size': update_node_size(radio_nodes_size,node), 'title': title, 'color': COLORS[node[radio_nodes_color]]}})
         graphdata = {'nodes': nodes, 'edges': edges}
         return visdcc.Network(id='graph', data=graphdata, options=OPTIONS)
     else:
